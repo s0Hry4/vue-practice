6c420a801c30f8a0f3034cf51d9bbf33b9704e31
s_sonoda <>
2019-02-07 11:03:11 +0900

    Handling User Input

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 var app = new Vue({ 
     el: '#app',
     data: {
-        message: '三犬'
+        message: '三鳥'
+    },
+    methods: {
+        changeMonstar: function () {
+            this.message = this.message.split('').reverse().join('');
+            list.monstars = [
+                    { name: 'ファイヤー' },
+                    { name: 'サンダー' },
+                    { name: 'フリーザー' }
+                ];
+        }
     }
 });
 
 var list = new Vue({
   el: '#list',
   data: {
-    todos: [
-      { text: 'エンテイ' },
-      { text: 'ライコウ' },
-      { text: 'スイクン' }
+    monstars: [
+      { name: 'エンテイ' },
+      { name: 'ライコウ' },
+      { name: 'スイクン' }
     ]
   }
 });
