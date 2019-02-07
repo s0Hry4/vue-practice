302f818a10bb13b3e627aedb4d7c053c6eecc0ff
s_sonoda <>
2019-02-07 10:54:34 +0900

    For loops

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var app = new Vue({ 
     el: '#app',
     data: {
-        message: 'こんにちは!'
+        message: '三犬'
     }
+});
+
+var list = new Vue({
+  el: '#list',
+  data: {
+    todos: [
+      { text: 'エンテイ' },
+      { text: 'ライコウ' },
+      { text: 'スイクン' }
+    ]
+  }
 });
